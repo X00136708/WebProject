400003a4108a66aac90fc3f5361ba86bc20cf7d8
Grizzly232 <michael.brady.ie@gmail.com>
2018-12-13 19:20:57 +0000

    testing

---
+++ .next/static/WLKIeER_i4cgB1ox0WUTU/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{252:function(e,n,t){__NEXT_REGISTER_PAGE("/about",function(){return e.exports=t(253),{page:e.exports.default}})},253:function(e,n,t){"use strict";t.r(n);var u=t(0),o=t.n(u);t(15);n.default=function(e){return o.a.createElement("div",null,o.a.createElement("p",null,"About Next.js"))}}},[[252,1,0]]]);
